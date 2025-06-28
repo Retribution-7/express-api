64ab264f77b1d076aa1341fecad909159484693d
Антон <antonradkov655@gmail.com>
2025-06-28 10:51:19 +0300

    Промежуточные обработчики

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import express from "express";
+import { userRouter } from './users/users.js'
 
 const port = 8000;
 const app = express();
 
-app.get('/hello', (req, res) => {
-  res.send('Привет');
+app.use((req, res, next) => {
+  console.log('Время ', Date.now());
+  next();
 })
+
+app.get('/hello', (req, res) => {
+  res.end();
+})
+
+app.use('/users', userRouter);
 
 app.listen(port, () => {
   console.log(`Сервер запущен на http://localhost:${port}`)
